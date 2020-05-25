9786f38e884465bb8db32cf0a5355d9c5f07c576
Gordon McKinney <gordon@night-ray.com>
2020-05-25 08:03:09 -0500

    fixing compile-time compatibility issues

--- Sources/main.swift
+++ Sources/main.swift
@@ -33,11 +33,4 @@ private func isTestRun() -> Bool {
     return NSClassFromString("XCTestCase") != nil
 }
 
-if isTestRun() {
-    // This skips setting up the app delegate
-    NSApplication.shared.run()
-} else {
-    // For some magical reason, the AppDelegate is setup when
-    // initialized this way
-    _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
-}
+NSApplication.shared.run()

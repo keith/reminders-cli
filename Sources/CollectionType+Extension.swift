f202ea62da0ec5b84553b8b08250583170f3c217
Gordon McKinney <gordon@night-ray.com>
2020-05-25 08:00:25 -0500

    fixing compile-time compatibility issues

--- Sources/CollectionType+Extension.swift
+++ Sources/CollectionType+Extension.swift
@@ -1,10 +1,10 @@
 extension Collection {
     func find(where predicate: (Iterator.Element) throws -> Bool) rethrows -> Iterator.Element? {
-        return try self.index(where: predicate).flatMap { self[$0] }
+        return try self.firstIndex(where: predicate).flatMap { self[$0] }
     }
 }
 
-extension Collection where Index == Int, IndexDistance {
+extension Collection where Index == Int {
     subscript(safe index: Int) -> Iterator.Element? {
         return index < self.count && index >= 0 ? self[index] : nil
     }

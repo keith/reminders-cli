e46ed799999f92f31609d44e892c6c523d06eeb4
Glenn 'devalias' Grant <glenn@devalias.net>
2023-01-03 19:11:43 +1100

    fix error: 'ISO8601FormatStyle' is only available in macOS 12.0 or newer

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "reminders",
     platforms: [
-        .macOS(.v10_15)
+        .macOS(.v12)
     ],
     products: [
         .executable(name: "reminders", targets: ["reminders"]),
